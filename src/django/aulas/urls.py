d7e52ea4f840e87f9137648f4101fbb9638793e6
Cesar Meloni <cesarmeloni@gmail.com>
2024-11-13 12:21:35 -0300

    Class 05 - Templates

--- src/django/aulas/urls.py
+++ src/django/aulas/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import nuevoHello, bye, edad
+from .views import nuevoHello, bye, edad, primer_plantilla, segunda_plantilla, tercer_plantilla
 
 urlpatterns = [
     path('hello', nuevoHello),
     path('bye', bye),
-    path('edad/<int:anios>/<int:futuro>', edad)
+    path('edad/<int:anios>/<int:futuro>', edad),
+    path('plantilla1', primer_plantilla),
+    path('plantilla2', segunda_plantilla),
+    path('plantilla3', tercer_plantilla),
 ]
